d445efb9c2c87897899425e33b55dfacbf50a461
RezaGalieva <95533022+RezaGalieva@users.noreply.github.com>
2022-06-27 22:50:12 +0300

    Task-7

--- popupform.js
+++ popupform.js
@@ -1,15 +1,23 @@
-const buttonSearch = document.querySelector(".button-search");
-const buttonFind = document.querySelector(".button-find");
 const modalPopup = document.querySelector(".modal-container");
-const popupForm = document.querySelector(".modal-popup");
-const inputArrival = document.querySelector(".input-arrival");
-const inputDeparture = document.querySelector(".input-departure");
-const buttonMinus = document.querySelector(".btn-minus");
-const buttonPlus = document.querySelector(".btn-plus");
-const adults = document.querySelector("#amount-adults");
-const buttonMinusChildren = document.querySelector(".btn-minus-ch");
-const buttonPlusChildren = document.querySelector(".btn-plus-ch");
-const children = document.querySelector("#amount-children");
+const buttonSearch = document.querySelector(".button-search");
+const popupForm = modalPopup.querySelector(".modal-popup");
+const buttonFind = popupForm.querySelector(".button-find");
+const inputArrival = popupForm.querySelector(".input-arrival");
+const inputDeparture = popupForm.querySelector(".input-departure");
+const buttonMinusAdults = popupForm.querySelector(
+  "#input-adults-container .btn-minus"
+);
+const buttonPlusAdults = popupForm.querySelector(
+  "#input-adults-container .btn-plus"
+);
+const inputAdults = popupForm.querySelector("#amount-adults");
+const buttonMinusChildren = popupForm.querySelector(
+  "#input-children-container .btn-minus"
+);
+const buttonPlusChildren = popupForm.querySelector(
+  "#input-children-container .btn-plus"
+);
+const inputChildren = popupForm.querySelector("#amount-children");
 
 buttonSearch.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -29,31 +37,31 @@ buttonFind.addEventListener("click", function (evt) {
   } else {
     localStorage.setItem("arrival", inputArrival.value);
     localStorage.setItem("departure", inputDeparture.value);
-    localStorage.setItem("adults", adults.value);
-    localStorage.setItem("children", children.value);
+    localStorage.setItem("adults", inputAdults.value);
+    localStorage.setItem("children", inputChildren.value);
   }
 });
 
-buttonMinus.addEventListener("click", function (evt) {
+buttonMinusAdults.addEventListener("click", function (evt) {
   evt.preventDefault();
-  if (adults.value > 1) {
-    adults.value = Number(adults.value) - 1;
+  if (inputAdults.value > 1) {
+    inputAdults.value = Number(inputAdults.value) - 1;
   }
 });
 
-buttonPlus.addEventListener("click", function (evt) {
+buttonPlusAdults.addEventListener("click", function (evt) {
   evt.preventDefault();
-  adults.value = Number(adults.value) + 1;
+  inputAdults.value = Number(inputAdults.value) + 1;
 });
 
 buttonMinusChildren.addEventListener("click", function (evt) {
   evt.preventDefault();
-  if (children.value > 0) {
-    children.value = Number(children.value) - 1;
+  if (inputChildren.value > 0) {
+    inputChildren.value = Number(inputChildren.value) - 1;
   }
 });
 
 buttonPlusChildren.addEventListener("click", function (evt) {
   evt.preventDefault();
-  children.value = Number(children.value) + 1;
+  inputChildren.value = Number(inputChildren.value) + 1;
 });
